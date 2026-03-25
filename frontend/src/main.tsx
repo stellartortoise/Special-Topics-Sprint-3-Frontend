c04ea4f848d318557e0e1f4ec5a276255faa2421
apb-nscc <86730691+stellartortoise@users.noreply.github.com>
2026-03-24 21:15:00 -0300

    Added React Router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router";
 import './index.css'
-import App from './App.tsx'
+//import App from './App.tsx'
+import Home from './Home.tsx'
+import Cart from './Cart.tsx'
+import Checkout from './Checkout.tsx'
+import Confirmation from "./Confirmation.tsx";
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/checkout" element={<Checkout />} />
+                <Route path="/confirmation" element={<Confirmation />} />
+            </Routes>
+        </BrowserRouter>
+    </StrictMode>,
 )
